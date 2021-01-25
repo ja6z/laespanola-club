c57d1d96355a1684e60196e92854e614478acaff
joel arraez <joelarraez214@gmail.com>
2021-01-25 00:27:16 -0500

    modificaciones de la plantilla predeterminada

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="section">
     <h2 class="title is-3 has-text-grey">
-      "Just start  <b-icon
+      "Siguenos en  <b-icon
         icon="rocket"
         size="is-large"
       />"
     </h2>
     <h3 class="subtitle is-6 has-text-grey">
-      Author: <a href="https://github.com/anteriovieira">
-        Antério Vieira
+      <a href="https://github.com/anteriovieira">
+        Facebook
       </a>
     </h3>
   </section>
